1a6bcb2523dd76b572260bbca4c8f84b6a4cbf28
bestct <977961764@qq.com>
2023-05-24 15:43:34 +0800

    完善网站文件依赖、替换消息提示插件

---
+++ client/src/main/resources/static/js/header.js
@@ -0,0 +1,81 @@
+(() => {
+    'use strict'
+    const storedTheme = localStorage.getItem('theme')
+    const getPreferredTheme = () => {
+        if (storedTheme) {
+            return storedTheme
+        }
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = function (theme) {
+        if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.setAttribute('data-bs-theme', 'dark')
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = theme => {
+        const activeThemeIcon = document.querySelector('.theme-icon-active use')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+        })
+
+        btnToActive.classList.add('active')
+        activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        if (storedTheme !== 'light' || storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    localStorage.setItem('theme', theme)
+                    setTheme(theme)
+                    showActiveTheme(theme)
+                })
+            })
+    })
+})()
+
+document.getElementById('searchBtn').addEventListener('click', function () {
+    //搜索内容不能为空
+    let keyword = document.getElementById('keyword').value
+    if (keyword === '' || keyword === undefined) {
+        Toastify({
+            text: "搜索内容不能为空",
+            duration: 2000,
+            newWindow: true,
+            close: false,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                //成功 linear-gradient(to right, #56ab2f, #a8e063)
+                //警告 linear-gradient(to right, #f2994a, #f2c94c)
+                //错误 linear-gradient(to right, #ed213a, #93291e)
+                //提示 linear-gradient(to right, #2c3e50, #bdc3c7)
+                background: "linear-gradient(to right, #f2994a, #f2c94c)",
+            },
+        }).showToast();
+    }else {
+
+    }
+})
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
--- client/src/main/resources/static/js/request.js
+++ client/src/main/resources/static/js/request.js
@@ -9,7 +9,22 @@ instance.defaults.headers.common["Authorization"] = getToken();
 instance.interceptors.response.use(function (response) {
     // 对响应数据做点什么
     if (response.data.code===500){
-        Qmsg.error(response.data.msg)
+        Toastify({
+            text: response.data.msg,
+            duration: 2000,
+            newWindow: true,
+            close: false,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                //成功 linear-gradient(to right, #56ab2f, #a8e063)
+                //警告 linear-gradient(to right, #f2994a, #f2c94c)
+                //错误 linear-gradient(to right, #ed213a, #93291e)
+                //提示 linear-gradient(to right, #2c3e50, #bdc3c7)
+                background: "linear-gradient(to right, #ed213a, #93291e)",
+            },
+        }).showToast();
         return Promise.reject(error);
     }
     return response.data;
