8143f707c8cc9c8e752c495b2807513ecb46fbd4
bestct <977961764@qq.com>
2023-07-20 11:17:46 +0800

    用vue重构格律检测页面

--- client/src/main/resources/static/js/check.js
+++ client/src/main/resources/static/js/check.js
@@ -1,70 +1,168 @@
 import instance from "./request.js";
+import {createApp} from './vue.esm-browser.prod.js'
+import {createToast} from "./base.js";
 
-const content = document.getElementById("content");
-const rhymeBookIds = document.getElementsByName('rhymeBookId')
-const selectedForm = document.getElementById("selectedForm")
-const selectedFirst = document.getElementById("selectedFirst")
-document.getElementById('searchHanZi').addEventListener('keydown', ev => {
-    if (ev.key === 'Enter') {
-        searchHanzi()
-    }
-})
-
-document.getElementById('searchZiBtn').addEventListener('click',ev => {
-    searchHanzi()
-})
-
-
-document.getElementById('checkBtn').onclick = function () {
-    let rhymeBookId
-    for (let i = 0; i < rhymeBookIds.length; i++) {
-        if (rhymeBookIds[i].checked === true) {
-            rhymeBookId = rhymeBookIds[i].value
-            break
+createApp
+({
+    created() {
+        document.getElementById("app").hidden = false;
+        this.loadForm()
+    },
+    data() {
+        return {
+            formHtml: "",
+            formsName: "",
+            searchResult: "",
+            searchHanZi: "",
+            selectedFormId: 1,
+            selectedFirstId: 1,
+            rhymeBookId: 1,
+            content:'',
+            selectedForm: [{id: 1, name: "五律"}, {id: 2, name: "五绝"}, {id: 3, name: "七律"}, {id: 4, name: "七绝"}],
+            selectedFirst: [{id: 1, name: "仄起首句不入韵"}, {id: 2, name: "仄起首句入韵"}, {id: 3, name: "平起首句不入韵"}, {
+                id: 4,
+                name: "平起首句入韵"
+            }],
+            formList: [
+                {formsCode: "31201,00114;20011,31104;31201,00114;20011,31104", formsId: "1-1", formsName: "五律·仄起首句不入韵"},
+                {formsCode: "31104,00114;20011,31104;31201,00114;20011,31104", formsId: "1-2", formsName: "五律·仄起首句入韵"},
+                {formsCode: "20011,31104;31201,00114;20011,31104;31001,00114", formsId: "1-3", formsName: "五律·平起首句不入韵"},
+                {formsCode: "00114,31104;31201,00114;20011,31104;31001,00114", formsId: "1-4", formsName: "五律·平起首句入韵"},
+                {formsCode: "31201,00114;20011,31104", formsId: "2-1", formsName: "五绝·仄起首句不入韵"},
+                {formsCode: "31124,00114;20011,31104", formsId: "2-2", formsName: "五绝·仄起首句入韵"},
+                {formsCode: "20011,31104;31201,00114", formsId: "2-3", formsName: "五绝·平起首句不入韵"},
+                {formsCode: "00114,31104;31201,00114", formsId: "2-4", formsName: "五绝·平起首句入韵"},
+                {
+                    formsCode: "3120011,2031104;2031201,3100114;3120011,2031104;2031201,3100114",
+                    formsId: "3-1",
+                    formsName: "七律·仄起首句不入韵"
+                },
+                {
+                    formsCode: "3100114,2031104;2031201,3100114;3120011,2031104;2031201,3100114",
+                    formsId: "3-2",
+                    formsName: "七律·仄起首句入韵"
+                },
+                {
+                    formsCode: "2031201,3100114;3120011,2031104;2031201,3100114;3120211,2031104",
+                    formsId: "3-3",
+                    formsName: "七律·平起首句不入韵"
+                },
+                {
+                    formsCode: "2031104,3100114;3120011,2031104;2031201,3100114;3120211,2031104",
+                    formsId: "3-4",
+                    formsName: "七律·平起首句入韵"
+                },
+                {formsCode: "3120011,2031104;2031201,3100114", formsId: "4-1", formsName: "七绝·仄起首句不入韵"},
+                {formsCode: "3100114,2031104;2031201,3100114", formsId: "4-2", formsName: "七绝·仄起首句入韵"},
+                {formsCode: "2031201,3100114;3120011,2031104", formsId: "4-3", formsName: "七绝·平起首句不入韵"},
+                {formsCode: "2031104,3100114;3120011,2031104", formsId: "4-4", formsName: "七绝·平起首句入韵"}
+            ]
         }
-    }
-    instance({
-            url: "/rhyme/check",
-            method: 'post',
-            data: {
-                checkContent: content.value,
-                formType: selectedForm.value,
-                firstType: selectedFirst.value,
-                rhymeBookId: rhymeBookId
+    },
+    //监听
+    watch: {
+        selectedFormId(newSelectedFormId, oldSelectedFormId) {
+            this.loadForm()
+        },
+        selectedFirstId(newSelectedFirstId, oldSelectedFirstId) {
+            this.loadForm()
+        }
+    },
+    methods: {
+        check() {
+            instance({
+                    url: "/rhyme/check",
+                    method: 'post',
+                    data: {
+                        checkContent: this.content,
+                        formType: this.selectedFormId,
+                        firstType: this.selectedFirstId,
+                        rhymeBookId: this.rhymeBookId
+                    }
+                }
+            ).then(res => {
+                document.getElementById('checkSuccess').hidden = false
+                document.getElementById("codeResult").innerHTML = res.data.codeResult;
+                document.getElementById('suggest').innerHTML = res.data.suggest
+            })
+        },
+        searchHanzi() {
+            instance({
+                    url: "/rhyme/search?hanZi=" + this.searchHanZi,
+                    method: 'get'
+                }
+            ).then(res => {
+                let HTML = "<div>"
+                document.getElementById('searchSuccess').hidden = false
+                for (const rhymeBook of res.data) {
+                    HTML += "<div style='padding: 10px'>"
+                    HTML += "<h4>" + rhymeBook.rhymeBookName + "</h4>"
+                    let rhymeList = rhymeBook.rhymeList
+                    HTML += "<div style='margin: 10px'>"
+                    for (const rhyme of rhymeList) {
+                        HTML += "<h5>"
+                        HTML += rhyme.rhymeName
+                        HTML += "</h5>"
+                        HTML += "<div style='letter-spacing:5px;line-height: 1.5;'>"
+                        HTML += rhyme.rhymeCharacter
+                        HTML += "</div>"
+                    }
+                    HTML += "</div>"
+                    HTML += "</div>"
+                }
+                HTML += "</div>"
+                this.searchResult = HTML;
+            })
+        },
+        loadForm() {
+            let formId = parseInt(this.selectedFormId) + "-" + parseInt(this.selectedFirstId);
+            let formCode = ""
+            let formsName = ""
+            var formList = this.formList
+            for (const key in formList) {
+                const element = formList[key];
+                if (element.formsId === formId) {
+                    formCode = element.formsCode
+                    formsName = element.formsName
+                    break
+                }
             }
-        }
-    ).then(res => {
-        document.getElementById('checkSuccess').hidden = false
-        document.getElementById("codeResult").innerHTML = res.data.codeResult;
-        document.getElementById('suggest').innerHTML = res.data.suggest
-    })
-}
-
-export default function searchHanzi() {
-    instance({
-            url: "/rhyme/search?hanZi=" + document.getElementById('searchHanZi').value,
-            method: 'get'
-        }
-    ).then(res => {
-        let HTML = "<div>"
-        document.getElementById('searchSuccess').hidden = false
-        for (const rhymeBook of res.data) {
-            HTML += "<div style='padding: 10px'>"
-            HTML += "<h4>" + rhymeBook.rhymeBookName + "</h4>"
-            let rhymeList = rhymeBook.rhymeList
-            HTML+="<div style='margin: 10px'>"
-            for (const rhyme of rhymeList) {
-                HTML+="<h5>"
-                HTML+=rhyme.rhymeName
-                HTML+="</h5>"
-                HTML+="<div style='letter-spacing:5px;line-height: 1.5;'>"
-                HTML+=rhyme.rhymeCharacter
-                HTML+="</div>"
+            let formHtml = ""
+            for (let i in formCode) {
+                let char = formCode[i]
+                if (char !== ';' && char !== ',') {
+                    switch (char) {
+                        case '0':
+                            formHtml += "平"
+                            break;
+                        case '1':
+                            formHtml += "仄"
+                            break;
+                        case '2':
+                            formHtml += "<span style='color: gray'>" + "平" + "</span>"
+                            break;
+                        case '3':
+                            formHtml += "<span style='color: gray'>" + "仄" + "</span>"
+                            break;
+                        case '4':
+                            formHtml += "<span style='color: purple'>" + "平" + "</span>"
+                            break;
+                        case '5':
+                            formHtml += "<span style='color: purple'>" + "仄" + "</span>"
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    if (char === ',') {
+                        formHtml += "，"
+                    } else {
+                        formHtml += "<br/>"
+                    }
+                }
             }
-            HTML+="</div>"
-            HTML += "</div>"
-        }
-        HTML += "</div>"
-        document.getElementById("searchResult").innerHTML = HTML;
-    })
-}
+            this.formHtml = formHtml
+            this.formsName = formsName
+        },
+    }
+}).mount('#app')
