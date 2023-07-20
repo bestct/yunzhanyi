dc72d368cafaddf84ecb5ea688d2c079ccbde515
bestct <977961764@qq.com>
2023-07-20 13:40:13 +0800

    配置404和500页面

---
+++ client/src/main/java/net/yunzhanyi/client/controller/MyErrorController.java
@@ -0,0 +1,56 @@
+package net.yunzhanyi.client.controller;
+
+import com.alibaba.fastjson.JSON;
+import net.yunzhanyi.common.core.vo.AjaxResult;
+import net.yunzhanyi.common.web.utils.AjaxUtils;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author bestct
+ * @date 2022/10/2
+ * @type 类
+ */
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        boolean isAjax = AjaxUtils.isAjax(request);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                if (isAjax){
+                    response.setCharacterEncoding("UTF-8");
+                    response.setContentType("application/json; charset=utf-8");
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    AjaxResult error = AjaxResult.error(404, "接口未找到");
+                    String string = JSON.toJSONString(error);
+                    response.getWriter().print(string);
+                    return null;
+                }else {
+                    return "redirect:/404";
+                }
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            }
+        }
+
+        // 这里后期可以扩展其他错误页面
+        return "404";
+    }
+
+    @GetMapping("/404")
+    public String page404(){
+        return "404";
+    }
+}
--- common/web/src/main/java/net/yunzhanyi/common/web/handler/GlobalExceptionHandler.java
+++ common/web/src/main/java/net/yunzhanyi/common/web/handler/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package net.yunzhanyi.common.web.handler;
 
 import cn.hutool.http.HttpStatus;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import net.yunzhanyi.common.core.exception.NotLoginException;
 import net.yunzhanyi.common.core.exception.NotPermissionException;
 import net.yunzhanyi.common.core.vo.AjaxResult;
+import net.yunzhanyi.common.web.utils.AjaxUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 全局异常处理程序
@@ -24,11 +29,32 @@ public class GlobalExceptionHandler {
      * 系统异常
      */
     @ExceptionHandler(Exception.class)
-    public AjaxResult handleException(Exception e, HttpServletRequest request)
-    {
+    public Object handleException(Exception exception, HttpServletRequest request,HttpServletResponse response) throws IOException {
         String requestURI = request.getRequestURI();
-        log.error("请求地址'{}',发生系统异常.", requestURI, e);
-        return AjaxResult.error(e.getMessage());
+        log.error("请求地址'{}',发生系统异常.", requestURI, exception);
+        boolean isAjax = AjaxUtils.isAjax(request);
+        if (isAjax) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json; charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            AjaxResult error = AjaxResult.error(500, exception.getMessage());
+            String string = JSON.toJSONString(error);
+            response.getWriter().print(string);
+            return null;
+        } else {
+            if (exception instanceof NullPointerException){
+                try {
+                    response.sendRedirect("/404");
+                    return null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            ModelAndView mv = new ModelAndView();
+            mv.addObject("errorMessage", exception.getMessage());
+            mv.setViewName("500");
+            return mv;
+        }
     }
     /**
      * 处理不允许异常
