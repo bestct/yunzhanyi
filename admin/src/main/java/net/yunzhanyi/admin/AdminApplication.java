20132fb6b670502625184b224681c768d05202e5
bestct <977961764@qq.com>
2023-07-29 12:00:21 +0800

    整合domain包，mybatis-plus融合mybatis

--- admin/src/main/java/net/yunzhanyi/admin/AdminApplication.java
+++ admin/src/main/java/net/yunzhanyi/admin/AdminApplication.java
@@ -1,5 +1,6 @@
 package net.yunzhanyi.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2023/04/20
  */
 @SpringBootApplication
+@MapperScan("net.yunzhanyi.**.mapper")
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class,args);
